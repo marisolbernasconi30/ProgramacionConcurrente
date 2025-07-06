551d26ddf160a4e5a0fb1588346df22c34455f9d
Marisol Bernasconi <usa2018sb1030@gmail.com>
2025-07-06 02:15:37 -0300

    sincronizacion entre los propios hilos

--- usosThread/SincronizacionThreads2.java
+++ usosThread/SincronizacionThreads2.java
@@ -5,20 +5,10 @@ public class SincronizacionThreads2 {
 
 
         HilosVarios elhilo1=new HilosVarios();
-        HilosVarios elhilo2=new HilosVarios();
-        elhilo1.start();
-        try {
-            elhilo1.join(); //ESTO ES LO DE SINCRONIZACION. NO HACE EL HILO 2 HASTA QUE EL 1 MUERA
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        HilosVarios2 elhilo2=new HilosVarios2(elhilo1); //el hilo2 no empieza hasta que termine el 1
         elhilo2.start();
-        try {
-            elhilo2.join(); 
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
+        elhilo1.start();
+        
         System.out.println("Fin de los hilos"); //ESTO CUENTA COMO HILO 3
 
     }
@@ -31,4 +21,24 @@ class HilosVarios extends Thread{ //OTRO MÉTODO PARA CREAR UN HILO
         }
 
     }
+}
+
+class HilosVarios2 extends Thread{ //OTRO MÉTODO PARA CREAR UN HILO
+    public HilosVarios2(Thread hilo){
+        this.hilo=hilo;
+
+    }
+    public void run(){
+
+        try {
+            hilo.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for(int i=0; i<15; i++ ){
+            System.out.println("Ejecutando hilo " + getName());
+        }
+
+    }
+    private Thread hilo;
 }
